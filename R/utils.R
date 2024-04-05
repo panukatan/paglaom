8060fcf2fc6cd04f227f09f6bcd3ee6f253db318
Ernest Guevarra <5742010+ernestguevarra@users.noreply.github.com>
2024-04-05 18:25:45 +0100

    Initial commit

---
+++ R/utils.R
@@ -0,0 +1,34 @@
+#' 
+#' Collect all targets and lists of targets in the environment
+#' 
+#' 
+all_targets <- function(env = parent.env(environment()), 
+                        type = "tar_target", 
+                        add_list_names = TRUE) {
+  
+  ## Function to determine if an object is a type (a target), 
+  ## or a list on only that type
+  rfn <- function(obj) 
+    inherits(obj, type) || (is.list(obj) && all(vapply(obj, rfn, logical(1))))
+  
+  ## Get the names of everything in the environment 
+  ## (e.g. sourced in the _targets.R file)
+  objs <- ls(env)
+  
+  out <- list()
+  for (o in objs) {
+    obj <- get(o, envir = env)      ## Get each top-level object in turn
+    if (rfn(obj)) {                 ## For targets and lists of targets
+      out[[length(out) + 1]] <- obj ## Add them to the output
+      
+      ## If the object is a list of targets, add a vector of the target names 
+      ## to the environment So that one can call `tar_make(list_name)` to make 
+      ## all the targets in that list
+      if (add_list_names && is.list(obj)) {
+        target_names <- vapply(obj, \(x) x$settings$name, character(1))
+        assign(o, target_names, envir = env)
+      }
+    }
+  }
+  return(out)
+}
---
+++ packages.R
@@ -0,0 +1,16 @@
+################################################################################
+#
+# Load packages required for standard workflows
+#
+################################################################################
+
+library(targets)
+library(tarchetypes)
+library(here)
+library(rmarkdown)
+library(knitr)
+library(kableExtra)
+library(dplyr)
+library(tidyr)
+library(openxlsx)
+library(ggplot2)
